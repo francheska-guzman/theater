c360532811b87f5cc2f9e930035706de9207918c
Francheska <francheska-guzman@users.noreply.github.com>
2018-02-06 05:41:19 -0500

    Add app name.

--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -5,10 +5,13 @@ import { NavLink } from 'react-router-dom';
 class Navigation extends Component {
   render() {
     return (
-      <nav>
-        <NavLink to="/theater">Home</NavLink>
-        <NavLink to="/theater/movie">Movie</NavLink>
-      </nav>
+      <div>
+        <span id="nav-name">Theater</span>
+        <nav>
+          <NavLink to="/theater">Home</NavLink>
+          <NavLink to="/theater/movie">Movie</NavLink>
+        </nav>
+      </div>
     );
   }
 }
